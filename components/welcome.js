b8ab8b020b258d8173afcb2354f1366a3a7ff78d
Fredderico Castro <ycongtv@gmail.com>
2025-04-03 00:43:25 +0800

    :turtle:

--- components/welcome.js
+++ components/welcome.js
@@ -3,7 +3,7 @@ import JSLogo from '../icons/javascript.svg';
 export default function Welcome(root) {
   const component = document.createElement('h1');
   component.innerHTML = `
-      <img src="${JSLogo}" alt="Vanilla JavaScript" />
+      <img src="./icons/javascript.svg" alt="Vanilla JavaScript" />
       Welcome to Example App!
   `;
 
